fef8418d1d8567c381f0fc07b18c08e0a53e6784
asmitt8373 <austinsmittle8373@gmail.com>
2024-12-15 12:26:28 -0800

    fixed package.json

--- server.js
+++ server.js
@@ -289,7 +289,10 @@ fastify.get("/admin", async (request, reply) => {
 
 // Run the server!
 try {
-  await fastify.listen({ port: 3000 });
+  await fastify.listen({
+    host: "0.0.0.0",
+    port: 3000,
+  });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
